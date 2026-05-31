fd8221e7d17d02c7837769f9b4065ccca1138c40
bensalemAlaa <alaahadjerbens@gmail.com>
2026-05-31 19:07:49 +0100

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -15,7 +15,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # Your Vercel frontend URL
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
